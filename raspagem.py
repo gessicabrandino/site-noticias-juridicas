98f12e31d146fa63cf8ce45f2a807c3ce2dc40bf
gessicabrandino <114264897+gessicabrandino@users.noreply.github.com>
2023-04-09 21:13:17 -0300

    Update raspagem.py

--- raspagem.py
+++ raspagem.py
@@ -1,8 +1,24 @@
+import os
+
 import requests
 import bs4
 import datetime
 import pandas as pd
+import gspread
+import oauth2client
 from bs4 import BeautifulSoup
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+
+#Conectando com a service account por meio da leitura do arquivo em Json
+
+GOOGLE_SHEETS_CREDENTIALS = os.environ['GOOGLE_SHEETS_CREDENTIALS']
+with open('credenciais.json', mode='w') as arquivo:
+  arquivo.write(GOOGLE_SHEETS_CREDENTIALS)
+conta = ServiceAccountCredentials.from_json_keyfile_name('credenciais.json')
+api = gspread.authorize(conta)
+
 
 #Acessando a página de última notícias da Folha
 
@@ -32,3 +48,26 @@ def filtro(ultimas_folha):
   return folhajus
   
   filtro(raspagem())
+  
+#Acessando a planilha de trabalho
+
+GOOGLE_SHEETS_KEY = os.environ['GOOGLE_SHEETS_KEY'] 
+planilha = api.open_by_key(f'{GOOGLE_SHEETS_KEY}') 
+sheet = planilha.worksheet('noticias')
+
+#Verificando o que já está registrado na planilha
+
+def noticias_novas(folhajus):
+  checagem = sheet.get_all_values() 
+  lista_envio=[]
+  for n in folhajus:
+    if n not in checagem:
+      lista_envio.append(n)
+      checagem.append(n)
+      sheet.append_rows([n])
+  df_lista_envio = pd.DataFrame(lista_envio)
+  df_lista_envio.columns=['Manchete','Link','Data']
+  df_lista_envio.set_index('Manchete')
+  return df_lista_envio
+  
+noticias_novas(filtro(raspagem()))
