20bf4a94bad61c13647e7e385d946a35b4fe1ecb
gessicabrandino <114264897+gessicabrandino@users.noreply.github.com>
2023-04-09 21:39:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,8 @@
-import os
-
-import gspread
-import oauth2client
 import requests
-import bs4
-import datetime
-import pandas as pd
-import sendgrid
-from bs4 import BeautifulSoup
 from flask import Flask, request
-from oauth2client.service_account import ServiceAccountCredentials
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail, Email, To, Content
 
-
-#Fazendo a conexão com o Google Sheets
-
-GOOGLE_SHEETS_KEY = os.environ['GOOGLE_SHEETS_KEY'] 
-
-GOOGLE_SHEETS_CREDENTIALS = os.environ['GOOGLE_SHEETS_CREDENTIALS']
-with open('credenciais.json', mode='w') as arquivo:
-  arquivo.write(GOOGLE_SHEETS_CREDENTIALS)
-conta = ServiceAccountCredentials.from_json_keyfile_name('credenciais.json')
-
-api = gspread.authorize(conta)
-planilha = api.open_by_key(f'{GOOGLE_SHEETS_KEY}') 
-sheet = planilha.worksheet('noticias')
-
-#Fazendo a conexão com o Sendgrid
-
-SENDGRID_API_KEY = os.environ['SENDGRID_API_KEY']
-carteiro = sendgrid.SendGridAPIClient(api_key = SENDGRID_API_KEY)
-
+from raspagem import raspa, filtro, noticias_novas
+from email import envia_email
 
 #Configurando o site com o Flask
 
@@ -57,11 +28,9 @@ def contato():
 @app.route("/coleta") #Função para raspar e filtras notícias
 def coleta():
   
-
-@app.route("/arquivo") #Função para guardar as notícias em uma planilha e gerar a lista de envio
-def arquivo():
- 
-
 @app.route("/carteiro") #Função para formatar a mensagem em html e mandar por email
 def carteiro():
+  df = noticias_novas(filtro(raspagem()))
+  envia_email(df)
+  return 'E-mail enviado'
 
